a531b366733b3738a8ec1fe56f39e736207a09ef
HoteaEmanuel <emanuelhotea1@gmail.com>
2025-03-15 18:07:21 +0200

    new js

--- CSS/Js/create_fund.js
+++
@@ -1,7 +0,0 @@
-function addDiv() {
-    const newDiv = document.createElement("div");
-    newDiv.classList.add("new-div");
-    newDiv.textContent = "New Div!";
-    
-    document.getElementById("container").appendChild(newDiv);
-}
